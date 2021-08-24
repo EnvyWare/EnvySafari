c75f4f288469e9cc8c7bdc9a702231da8263aaee
danorris709 <dnorris443@gmail.com>
2021-08-24 09:00:23 +0100

    Fix: NPE / Allows for nucleus unloading worlds

--- forge/src/main/java/com/envyful/pixel/forge/listener/NPCInteractListener.java
+++ forge/src/main/java/com/envyful/pixel/forge/listener/NPCInteractListener.java
@@ -2,6 +2,7 @@ package com.envyful.pixel.forge.listener;
 
 import com.envyful.api.forge.chat.UtilChatColour;
 import com.envyful.api.forge.listener.LazyListener;
+import com.envyful.api.forge.world.UtilWorld;
 import com.envyful.pixel.forge.PixelSafariForge;
 import com.envyful.pixel.forge.player.PixelSafariAttribute;
 import com.pixelmonmod.pixelmon.Pixelmon;
@@ -13,6 +14,7 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.text.TextComponentString;
+import net.minecraft.world.World;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -38,8 +40,16 @@ public class NPCInteractListener extends LazyListener {
         event.setCanceled(true);
         event.setCancellationResult(EnumActionResult.PASS);
 
+        World world = UtilWorld.findWorld(this.mod.getConfig().getWorldName());
+
         EntityPlayerMP player = (EntityPlayerMP) event.getEntityPlayer();
         PixelSafariAttribute attribute = this.mod.getPlayerManager().getPlayer(player).getAttribute(PixelSafariForge.class);
+
+        if (world == null) {
+            player.sendMessage(new TextComponentString(UtilChatColour.translateColourCodes('&',
+                    "&c&l(!) &cThe safari is currently closed!")));
+            return;
+        }
 
         if (attribute == null || attribute.inSafari()) {
             return;
