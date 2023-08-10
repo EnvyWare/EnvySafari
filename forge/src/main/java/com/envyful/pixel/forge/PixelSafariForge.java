06928dff5464414e1c3d76fb633575706c1adb87
DevSolaris <solaris.dev.2002@gmail.com>
2023-08-10 21:38:56 +0200

    chore(gradle) bump forge to 36.2.39, api to 4.9.0, Pixelmon to 9.1.6 and shadow to 6.1.0

--- forge/src/main/java/com/envyful/pixel/forge/PixelSafariForge.java
+++ forge/src/main/java/com/envyful/pixel/forge/PixelSafariForge.java
@@ -26,7 +26,7 @@ import java.io.IOException;
 public class PixelSafariForge {
 
     public static final String NPC_NBT = "PIXEL_SAFARI_NPC";
-    public static final String VERSION = "1.1.5";
+    public static final String VERSION = "1.1.6";
 
     private static PixelSafariForge instance;
 
