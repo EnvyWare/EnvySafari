48971a7144084ed262bfc53475332314f0eebb28
Daniel Norris <dnorris443@gmail.com>
2023-08-30 10:44:47 +0100

    feat(forge20): port finish task handling

---
+++ forge20/src/main/java/com/envyful/pixel/forge/task/CheckSafariFinishTask.java
@@ -0,0 +1,34 @@
+package com.envyful.pixel.forge.task;
+
+import com.envyful.api.forge.concurrency.UtilForgeConcurrency;
+import com.envyful.api.forge.player.ForgeEnvyPlayer;
+import com.envyful.pixel.forge.PixelSafariForge;
+import com.envyful.pixel.forge.player.PixelSafariAttribute;
+
+public class CheckSafariFinishTask implements Runnable {
+
+    private final PixelSafariForge mod;
+
+    public CheckSafariFinishTask(PixelSafariForge mod) {
+        this.mod = mod;
+    }
+
+    @Override
+    public void run() {
+        for (ForgeEnvyPlayer onlinePlayer : this.mod.getPlayerManager().getOnlinePlayers()) {
+            if (onlinePlayer == null) {
+                continue;
+            }
+
+            PixelSafariAttribute attribute = onlinePlayer.getAttribute(PixelSafariForge.class);
+
+            if (attribute == null || !attribute.inSafari()) {
+                continue;
+            }
+
+            if (attribute.shouldSafariFinish()) {
+                UtilForgeConcurrency.runSync(attribute::finishSafari);
+            }
+        }
+    }
+}
