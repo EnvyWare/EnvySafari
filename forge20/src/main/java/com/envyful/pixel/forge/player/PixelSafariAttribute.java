f70c906e9a0dddfc09c662ca0764c9e756b92e16
Daniel Norris <dnorris443@gmail.com>
2023-08-30 10:45:29 +0100

    feat(forge20): port safari attribute

---
+++ forge20/src/main/java/com/envyful/pixel/forge/player/PixelSafariAttribute.java
@@ -0,0 +1,122 @@
+package com.envyful.pixel.forge.player;
+
+import com.envyful.api.config.type.ConfigItem;
+import com.envyful.api.forge.chat.UtilChatColour;
+import com.envyful.api.forge.config.UtilConfigItem;
+import com.envyful.api.forge.player.ForgePlayerManager;
+import com.envyful.api.forge.player.attribute.AbstractForgeAttribute;
+import com.envyful.api.forge.player.inventory.InventorySnapshot;
+import com.envyful.api.forge.player.util.UtilTeleport;
+import com.envyful.api.forge.server.UtilForgeServer;
+import com.envyful.api.forge.world.UtilWorld;
+import com.envyful.pixel.forge.PixelSafariForge;
+import com.envyful.pixel.forge.config.PixelSafariConfig;
+import com.pixelmonmod.pixelmon.api.economy.BankAccount;
+import com.pixelmonmod.pixelmon.api.economy.BankAccountProxy;
+import org.joml.Vector3d;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class PixelSafariAttribute extends AbstractForgeAttribute<PixelSafariForge> {
+
+    private long safariEnd = -1;
+    private BankAccount bankAccount = null;
+    private boolean inSafari = false;
+    private InventorySnapshot inventorySnapshot;
+
+    public PixelSafariAttribute(PixelSafariForge manager, ForgePlayerManager playerManager) {
+        super(manager, playerManager);
+    }
+
+    public boolean inSafari() {
+        return this.inSafari;
+    }
+
+    public boolean shouldSafariFinish() {
+        return System.currentTimeMillis() > this.safariEnd;
+    }
+
+    public void startSafari() {
+        this.inSafari = true;
+        this.startSafari(this.manager.getConfig().getMainZone());
+    }
+
+    public void startSafari(String zone) {
+        PixelSafariConfig.ZoneInfo zoneInfo = this.getZone(zone);
+
+        if (zoneInfo == null) {
+            return;
+        }
+
+        this.getBankAccount().take(this.manager.getConfig().getCost());
+        this.safariEnd = System.currentTimeMillis() +
+                TimeUnit.SECONDS.toMillis(this.manager.getConfig().getTimeInSeconds());
+
+        for (String startCommmand : this.manager.getConfig().getStartCommmands()) {
+            UtilForgeServer.executeCommand(startCommmand.replace("%player%", this.parent.getParent().getName().getString()));
+        }
+
+        if (this.manager.getConfig().getSafariSettings().isCacheInventory()) {
+            this.inventorySnapshot = InventorySnapshot.of(this.parent.getParent());
+            this.parent.getParent().getInventory().clearContent();
+
+            List<ConfigItem> temporaryItems = this.manager.getConfig().getSafariSettings().getTemporaryItems();
+
+            for (ConfigItem temporaryItem : temporaryItems) {
+                this.parent.getParent().getInventory().add(UtilConfigItem.fromConfigItem(temporaryItem));
+            }
+
+            this.parent.getParent().initMenu(this.parent.getParent().inventoryMenu);
+        }
+
+        var world = UtilWorld.findWorld(this.manager.getConfig().getWorldName());
+        UtilTeleport.teleportPlayer(this.parent.getParent(), world,
+                new Vector3d(zoneInfo.getX() + 0.5, zoneInfo.getY(), zoneInfo.getZ() + 0.5),
+                zoneInfo.getPitch(), zoneInfo.getYaw());
+        this.parent.message(UtilChatColour.translateColourCodes('&',"&c&l- &c$" + this.manager.getConfig().getCost()));
+    }
+
+    private PixelSafariConfig.ZoneInfo getZone(String zone) {
+        for (PixelSafariConfig.ZoneInfo zoneInfo : this.manager.getConfig().getZones()) {
+            if (zoneInfo.getName().equalsIgnoreCase(zone)) {
+                return zoneInfo;
+            }
+        }
+
+        return null;
+    }
+
+    public boolean hasEnoughMoney() {
+        return this.getBankAccount().getBalance().doubleValue() >= this.manager.getConfig().getCost();
+    }
+
+    private BankAccount getBankAccount() {
+        if (this.bankAccount == null) {
+            this.bankAccount = BankAccountProxy.getBankAccountUnsafe(this.parent.getUuid());
+        }
+
+        return this.bankAccount;
+    }
+
+    public void finishSafari() {
+        PixelSafariConfig.ZoneInfo zoneInfo = this.manager.getConfig().getSpawnPosition();
+
+        if (this.manager.getConfig().getSafariSettings().isCacheInventory()) {
+            this.inventorySnapshot.restore(this.parent.getParent());
+        }
+
+        this.inSafari = false;
+        this.safariEnd = -1;
+        this.parent.message(this.manager.getLocale().getReturningToSpawn());
+        UtilTeleport.teleportPlayer(this.parent.getParent(), UtilWorld.findWorld(this.manager.getConfig().getSpawnWorldName()),
+                new Vector3d(zoneInfo.getX() + 0.5, zoneInfo.getY(), zoneInfo.getZ() + 0.5),
+                zoneInfo.getPitch(), zoneInfo.getYaw());
+    }
+
+    @Override
+    public void load() {}
+
+    @Override
+    public void save() {}
+}
