d7ace92c95c6b96f0c101d2fe299f7d87fdea9a2
Daniel Norris <dnorris443@gmail.com>
2023-10-19 15:39:27 +0100

    chore(gradle): bump to 1.3.0

--- forge/src/main/java/com/envyful/pixel/forge/PixelSafariForge.java
+++ forge/src/main/java/com/envyful/pixel/forge/PixelSafariForge.java
@@ -26,7 +26,7 @@ import java.io.IOException;
 public class PixelSafariForge {
 
     public static final String NPC_NBT = "PIXEL_SAFARI_NPC";
-    public static final String VERSION = "1.2.0";
+    public static final String VERSION = "1.3.0";
 
     private static PixelSafariForge instance;
 
